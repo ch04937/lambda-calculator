f319dc49044621fc751538e264578c4007f21971
ch04937 <97hernandez.c@gmail.com>
2019-07-16 16:17:49 -0700

    'started step 3'

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -4,7 +4,7 @@ import React from "react";
 import React, {useState} from 'react'
 
 //Import your array data to from the provided data file
-import {numbers} from './data.js';
+import {numbers} from '../../../data';
 
 const Numbers = () => {
   // STEP 2 - add the imported data to state
@@ -14,6 +14,8 @@ const Numbers = () => {
       {/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
        it any props needed by the child component*/}
+      {numbers.map(()=> <button >{setNumber}</button>)}
+
     </div>
   );
 };
--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,10 +1,15 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
+  const buttonContent = props;
+
   return (
     <>
       {/* Display a button element rendering the data being passed down from the parent container on props */}
+      <button className='operatorButton'>{buttonContent}</button>
+
     </>
+
   );
 };
 export default OperatorButton;
